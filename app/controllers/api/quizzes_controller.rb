43de9be9d52c64a311ec02b3f8d94a92db3fec5c
Leejojo <noodle.lee@gmail.com>
2016-08-11 11:57:51 -0400

    add serializer to output JSON
    add active model serializer to output JSON

--- app/controllers/api/quizzes_controller.rb
+++ app/controllers/api/quizzes_controller.rb
@@ -2,7 +2,8 @@ module Api
   class QuizzesController < ApiController
 
     def show
-      
+      quiz = Quiz.find(params[:id])
+      render json: quiz.as_json(include: {categories: {include: {questions: {include: {choices: {only: :text}}}}}})
     end
 
   end
